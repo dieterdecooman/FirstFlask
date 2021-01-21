a571eed7ac6f2c055a629e920fd0889fe1ea1705
dieterdecooman <73192452+dieterdecooman@users.noreply.github.com>
2021-01-21 20:34:02 +0100

    Create flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,16 @@
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html',active='home')
+
+@app.route('/link')
+def link():
+    return render_template('index.html',active='link')
+
+if __name__ == '__main__':
+    app.run()
